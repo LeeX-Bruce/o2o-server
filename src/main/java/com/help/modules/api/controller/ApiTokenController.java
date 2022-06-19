c19e38b5236bee7afa4946234f4c5ee0541ce7e7
limeihua <limhua23@foxmail.com>
2022-06-19 21:36:17 +0800

    增加checkToken接口

--- src/main/java/com/help/modules/api/controller/ApiTokenController.java
+++ src/main/java/com/help/modules/api/controller/ApiTokenController.java
@@ -1,9 +1,15 @@
 package com.help.modules.api.controller;
 
+import com.help.common.utils.R;
+import com.help.modules.api.entity.TokenEntity;
 import com.help.modules.api.service.TokenService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 /**
  * @author LMH
@@ -12,11 +18,21 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-@RequestMapping("/api/checkToken")
+@RequestMapping("/api")
 @Api("Token接口")
 public class ApiTokenController {
 
     private TokenService tokenService;
 
-
+    @GetMapping("/checkToken")
+    @ApiOperation(value = "检查Token是否失效")
+    public R checkToken(TokenEntity token) {
+        //比较当前时间，看token是否过期
+        if(token.getExpireTime().after(new Date())){
+            return R.ok();
+        }
+        else{
+            return R.error();
+        }
+    }
 }
